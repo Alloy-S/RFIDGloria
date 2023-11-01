020bf53023b12d4da598e9f868f5ec4aaf938efb
Alloy-S <c14210265@john.petra.ac.id>
2023-11-01 11:49:50 +0700

    table history

--- history.php
+++ history.php
@@ -1,3 +1,11 @@
+<?php
+require("./conn.php");
+
+$stmt = $conn->prepare("SELECT * FROM history");
+$stmt->execute();
+$rows = $stmt->fetchAll();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,9 +25,10 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <style>
         .logo-gloria {
-           border-radius: 50%; 
+            border-radius: 50%;
         }
     </style>
 
@@ -86,7 +95,7 @@
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="history.php">
+                <a class="nav-link" href="#">
                     <i class="fas fa-fw fa-history"></i>
                     <span>Riwayat Penjemputan</span></a>
             </li>
@@ -126,7 +135,46 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-3 mt-4 text-gray-800">Riwayat Kendaraan</h1>
 
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Kelas</th>
+                                            <th>Kendaraan</th>
+                                            <th>Waktu Datang</th>
+                                            <th>Waktu Pergi</th>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Kelas</th>
+                                            <th>Kendaraan</th>
+                                            <th>Waktu Datang</th>
+                                            <th>Waktu Pergi</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php foreach($rows as $row):?>
+                                        <tr>
+                                            <td>Budi</td>
+                                            <td>7-A</td>
+                                            <td>Kijang Innova</td>
+                                            <td><?= $row['entry_date']; ?></td>
+                                            <td><?= $row['exit_time']; ?></td>
+                                        </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
                 <!-- /.container-fluid -->
@@ -165,6 +213,10 @@
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
 
+    <!-- datatable pagination -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="./js/demo/datatables-demo.js"></script>
 </body>
 
 </html>
