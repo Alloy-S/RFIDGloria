36bb18dd1c105d82232226faa3b1fcb8be0b897f
Cheezeus <85939221+Cheezeus@users.noreply.github.com>
2023-11-12 12:07:38 +0700

    dashboard animation

--- index.php
+++ index.php
@@ -23,10 +23,63 @@ require_once('./conn.php');
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <script src="animation_dashboard.js"></script>
+
     <style>
         .logo-gloria {
            border-radius: 50%; 
         }
+
+        .header {
+            color: #0352A3;
+            font-size: 6em;
+            font-weight: bold;
+        }
+
+        .container-fluid {
+            animation: 1.5s anim-lineUP ease-in;
+        }
+
+        #admin {
+            font-size: 3em;
+            background-color: #0352A3;
+            color: white;
+            padding: 0 0.5em;
+        }
+
+        #welcome {
+            font-size: 4em;
+            letter-spacing: 0.05em;
+        }
+
+        .bg-dashboard-image {
+            background: url(./img/welcome-person.png);
+            background-position: center;
+            background-size: 100%;
+            background-repeat: no-repeat;
+            animation: 2s anim-lineUP ease-out;
+        }
+
+        @keyframes anim-lineUP {
+            0% {
+                opacity: 0;
+                transform: translateY(10%);
+            }
+            20% {
+                opacity: 0;
+            }
+            50% {
+                opacity: 1;
+                transform: translateY(0%);
+            }
+            100% {
+                opacity: 1;
+                transform: translateY(0%);
+            }
+        }
+
+
     </style>
 
 </head>
@@ -42,15 +95,22 @@ require_once('./conn.php');
         <div id="content-wrapper" class="d-flex flex-column">
 
             <!-- Main Content -->
-            <div id="content">
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-
-
+            <div class="row mt-5" id="content">
+                <div class="col-lg-7">
+                    <!-- Begin Page Content -->
+                    <div class="container-fluid">
+                            <div class="text-center mt-5 mx-4" id="text">
+                                <span class="header">Hello, </span>
+                                <span class="header" id="admin">Admin</span>
+                                <div class="header word" id="welcome"></div>
+                            </div>
+                    </div>
+                    <!-- /.container-fluid -->
                 </div>
-                <!-- /.container-fluid -->
+                
+                
+                <div class="col-lg-5 d-none d-lg-block bg-dashboard-image"></div>
 
             </div>
             <!-- End of Main Content -->
@@ -87,5 +147,11 @@ require_once('./conn.php');
     <script src="js/sb-admin-2.min.js"></script>
 
 </body>
+
+<script>
+    $(document).ready(function () {
+    wordflick();
+    });
+</script>
 
 </html>
